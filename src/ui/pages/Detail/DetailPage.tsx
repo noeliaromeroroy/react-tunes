3f032bc3c7248d88d4c214e5a0f59883f762264a
noeliaromeroroy <noeliaromeroroy@gmail.com>
2023-08-15 13:31:24 +0200

    fix/Detail: Added useEffect to handle setIsHome state update

--- src/ui/pages/Detail/DetailPage.tsx
+++ src/ui/pages/Detail/DetailPage.tsx
@@ -1,12 +1,14 @@
 import { useParams } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../../assets/styles/index.css';
 import { useSearch } from '../../contexts/PlayerContext';
 
 function Detail(): JSX.Element {
   const { id } = useParams();
   const { setIsHome } = useSearch();
-  setIsHome(false);
+  useEffect(() => {
+    setIsHome(false);
+  }, []);
   return (
     <div>
       <span>Esta es la página de detalle del podcast</span>
--- src/ui/pages/Search/SearchPage.tsx
+++ src/ui/pages/Search/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import '../../../assets/styles/index.css';
 import { useSearch } from '../../contexts/PlayerContext';
@@ -6,14 +6,17 @@ import { NavLink } from 'react-router-dom';
 
 function Search(): JSX.Element {
   const { results, setIsHome } = useSearch();
-  setIsHome(true);
-
+  useEffect(() => {
+    setIsHome(true);
+  }, []);
   return (
     <div>
       <div id="results">
         {results.map((podcast) => {
           return (
-            <NavLink to={`/detail/${podcast.id}`}>{podcast.title}</NavLink>
+            <NavLink to={`/detail/${podcast.id}`} key={podcast.id}>
+              {podcast.title}
+            </NavLink>
           );
         })}
       </div>
