a5c459cbb122899ae365cc16f7173968bd1daf1a
noeliaromeroroy <noeliaromeroroy@gmail.com>
2023-08-16 15:53:51 +0200

    feat: Enhance podcast player functionality
    - Implemented real-time podcast duration and playback time display.
    - Added functionality to seek specific playback positions.
    - Integrated volume control for playback.

--- src/ui/components/PlayerBar.tsx
+++ src/ui/components/PlayerBar.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Progress, Button } from '@material-tailwind/react';
+import { Avatar, Progress, Button, Slider } from '@material-tailwind/react';
 import Shufle from '../../assets/svg/shuffle-icon.svg';
 import Previous from '../../assets/svg/previous-icon.svg';
 import Pause from '../../assets/svg/pause-icon.svg';
@@ -8,18 +8,65 @@ import Next from '../../assets/svg/next-icon.svg';
 import Volume from '../../assets/svg/volume-icon.svg';
 import styles from './PlayerBar.module.css';
 import { usePlayerContext } from '../contexts/PlayerContext';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { formatTime } from '../helpers/dateHelper';
 
 const PlayerBar: React.FC = () => {
-  const { activePodcast, isPlaying, setIsPlaying } = usePlayerContext();
+  const { activePodcast, isPlaying, setIsPlaying, audio } = usePlayerContext();
+  const [currentTime, setCurrentTime] = useState('0:00');
+  const [duration, setDuration] = useState('0:00');
+  const [audioProgress, setAudioProgress] = useState(0);
+  const [volume, setVolume] = useState(20);
+  const isDragging = useRef(false);
 
-  function togglePlay() {
-    if (isPlaying) {
-      setIsPlaying(false);
-    } else {
-      setIsPlaying(true);
+  useEffect(() => {
+    if (audio) {
+      const updateDuration = () => {
+        setDuration(formatTime(audio.duration));
+      };
+
+      const handleTimeUpdate = () => {
+        if (!isDragging.current) {
+          setCurrentTime(formatTime(audio.currentTime));
+          setAudioProgress((audio.currentTime * 100) / audio.duration);
+        }
+      };
+
+      audio.addEventListener('loadedmetadata', updateDuration);
+      audio.addEventListener('timeupdate', handleTimeUpdate);
+
+      return () => {
+        audio.removeEventListener('timeupdate', handleTimeUpdate);
+        audio.removeEventListener('loadedmetadata', updateDuration);
+      };
     }
-  }
+  }, [audio]);
+
+  const handleSliderChange = (e: any) => {
+    const percentage = parseFloat(e.target.value);
+    if (audio) {
+      const newTime = (percentage / 100) * audio.duration;
+      audio.currentTime = newTime;
+
+      setAudioProgress(percentage);
+      setCurrentTime(formatTime(newTime));
+      if (percentage === 100) setIsPlaying(false);
+    }
+  };
+
+  const handleVolumeChange = (e: any) => {
+    if (audio) {
+      audio.volume = e.target.value / 100;
+      setVolume(audio.volume * 100);
+    }
+  };
+
+  const nextEpisode = () => {};
+  const previousEpisode = () => {};
+
+  const togglePlay = () => {
+    setIsPlaying((prevIsPlaying) => !prevIsPlaying);
+  };
 
   return (
     <div className={styles.PlayerBar}>
@@ -43,27 +90,35 @@ const PlayerBar: React.FC = () => {
         className={`${styles.controls} ${!activePodcast ? 'opacity-40' : ''}`}
       >
         <Shufle />
-        <Previous />
+        <Button onClick={() => previousEpisode()}>
+          <Previous />
+        </Button>
         <Button onClick={() => togglePlay()}>
           {isPlaying ? <Pause /> : <Play />}
         </Button>
-        <Next />
+        <Button onClick={() => nextEpisode()}>
+          <Next />
+        </Button>
+
         <Repeat />
         <div className={styles.progress}>
-          <span>03:41</span>
-          <Progress
-            value={25}
-            size="sm"
+          <span>{currentTime}</span>
+          <Slider
+            min="0"
+            max={duration}
+            value={audioProgress}
+            onChange={handleSliderChange}
             className={`${styles.progressBar} w-[400px]`}
           />
-          <span className={styles.time}>12:11</span>
+          <span className={styles.time}>{duration}</span>
         </div>
       </div>
       <div className={`${styles.volume} ${!activePodcast ? 'opacity-40' : ''}`}>
         <Volume />
-        <Progress
-          value={25}
+        <Slider
+          value={volume}
           size="sm"
+          onChange={handleVolumeChange}
           className={`${styles.progressBar} w-[100px]`}
         />
       </div>
--- src/ui/helpers/dateHelper.ts
+++ src/ui/helpers/dateHelper.ts
@@ -45,3 +45,9 @@ export const millisToMinutesAndSeconds = (millis: number) => {
         seconds.toString().padStart(2, '0')
     );
 };
+
+export const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
