6f9a6b0d850f329157a8256c64aaa727a15e102f
noeliaromeroroy <noeliaromeroroy@gmail.com>
2023-08-17 14:26:28 +0200

    enhancement: return to results page when searching from podcast detail view

--- src/ui/components/MainSearchBar.tsx
+++ src/ui/components/MainSearchBar.tsx
@@ -3,10 +3,11 @@ import Back from '../../assets/svg/back-icon.svg';
 import { Input } from '@material-tailwind/react';
 import { searchPodcasts } from '../../infrastructure/services/ITunesPodcastService';
 import { usePlayerContext } from '../contexts/PlayerContext';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import styles from './MainSearchBar.module.css';
 
 export const MainSearchBar: React.FC = () => {
+  const navigate = useNavigate();
   const { setResults, isHome, setFilteredResults } = usePlayerContext();
 
   const handleKeyDown = async (
@@ -18,6 +19,7 @@ export const MainSearchBar: React.FC = () => {
         const podcasts = await searchPodcasts(searchTerm);
         setResults(podcasts);
         setFilteredResults(podcasts);
+        navigate('/');
       }
     }
   };
